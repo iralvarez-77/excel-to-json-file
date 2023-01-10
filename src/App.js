80879e90d7285228c7cd99f10b3019505e0eb9b9
Ira Álvarez <irainelly.alvarez@comafi.com.ar>
2023-01-09 23:03:13 -0300

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as XLSX from "xlsx";
 
-function App() {
+export default function App() {
+
+  const [file, setFile] = useState();
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+
+    const data = await file.arrayBuffer();
+
+    const workbook = XLSX.read(data);
+
+    const worksheet = workbook.Sheets[workbook.SheetNames[1]];
+
+    const sheet = XLSX.utils.sheet_to_json(worksheet);
+    setFile(sheet);
+
+    console.log(sheet);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Converting file.xlsx to file.json</h1>
+      <form>
+        <input
+          type="file"
+          id="file"
+          onChange={handleChange}
+          accept=".xlsx"
+          multiple
+        />
+      </form>
+      {/* <ul>
+        {file?.map((item) => (
+          <li key={item.Cliente}>{item.Cliente}</li>
+        ))}
+      </ul> */}
+    </div> 
   );
 }
-
-export default App;
